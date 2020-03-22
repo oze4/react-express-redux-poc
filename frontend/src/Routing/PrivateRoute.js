f3515a80920d054ea5ee1998c89a1caf739ae351
Matt Oestreich <matthewpoestreich@gmail.com>
2020-03-21 21:33:50 -0500

    Update PrivateRoute.js

--- frontend/src/Routing/PrivateRoute.js
+++ frontend/src/Routing/PrivateRoute.js
@@ -3,19 +3,19 @@ import { withRedux } from "../Redux";
 
 import { Route, Redirect } from "react-router-dom";
 
-const PrivateRoute = ({ component: Component, state, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      state.isAccountVerified ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{ pathname: "/", state: { message: "Not Authorized!" } }}
-        />
-      )
-    }
-  />
-);
+const PrivateRoute = ({ component: Component, state, ...rest }) => {
+  if (!state.isAccountVerified) {
+    setTimeout(() => alert("Not authorized!"), 10);
+  }
+
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        state.isAccountVerified ? <Component {...props} /> : <Redirect to="/" />
+      }
+    />
+  );
+};
 
 export default withRedux(PrivateRoute);
